a2e2e798474706e27d29a6d08d9c3a684785f93a
Vijay <Vijay.anandr@icloud.com>
2025-02-23 09:50:51 -0700

    Initial Version for the Vijay OCP Test

---
+++ VijayJavaProjectforOCP/src/VijayJavaPkgforOCP/VijayHelloWorldforOCP.java
@@ -0,0 +1,19 @@
+package VijayJavaPkgforOCP;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class VijayHelloWorldforOCP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Vijay Welcome to the World of OCP");
+		 LocalDateTime CurrDateTime = LocalDateTime.now();
+		    System.out.println("CurrDateTime: " + CurrDateTime);
+		    DateTimeFormatter CurrDateTimeFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+
+		    String CurrDateTimeFormatted = CurrDateTime.format(CurrDateTimeFormat);
+		    System.out.println("CurrDateTimeFormatted: " + CurrDateTimeFormatted);
+	}
+
+}
