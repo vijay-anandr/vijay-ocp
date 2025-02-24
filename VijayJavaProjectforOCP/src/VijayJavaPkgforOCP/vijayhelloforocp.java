615bbab80decca2fa8780c75fa6c335eaf3a3bb9
Vijay <dearvar@gmail.com>
2025-02-23 22:36:31 -0700

    Updated working version

---
+++ VijayJavaProjectforOCP/src/VijayJavaPkgforOCP/vijayhelloforocp.java
@@ -0,0 +1,20 @@
+package VijayJavaPkgforOCP;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class vijayhelloforocp {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.println("Vijay Welcome to the World of OCP");
+			 LocalDateTime CurrDateTime = LocalDateTime.now();
+			    System.out.println("CurrDateTime: " + CurrDateTime);
+			    DateTimeFormatter CurrDateTimeFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+
+			    String CurrDateTimeFormatted = CurrDateTime.format(CurrDateTimeFormat);
+			    System.out.println("Current Sytem Date Time Formatted: " + CurrDateTimeFormatted);
+		}
+
+	}
